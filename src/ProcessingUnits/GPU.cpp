f1a804dd7e0f5d02ca4cc5ba820338b6ab55b49c
agrellier <alberto.grellier@epitech.eu>
2019-10-11 16:10:54 +0200

    Add read/write method for VRAM and OAM in GPU

--- src/ProcessingUnits/GPU.cpp
+++ src/ProcessingUnits/GPU.cpp
@@ -5,25 +5,14 @@
 ** Gpu.cpp
 */
 
-#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "GPU.hpp"
 #include "CPU.hpp"
 
-#define VRAM_SIZE 8 * 1024
-#define OAM_SIZE 159
-
 namespace GBEmulator
 {
 	GPU::GPU() : _vram(VRAM_SIZE, 0), _oam(OAM_SIZE, ROM_BANK_SIZE) {
-		sf::RenderWindow window(sf::VideoMode(160 * 4, 144 * 4), "GPU Test");
-		sf::Texture texture;
-		sf::Sprite sprite;
-		std::vector<sf::Color> colors;
-		colors.push_back(sf::Color::Black);
-		colors.push_back(sf::Color::Magenta);
-		colors.push_back(sf::Color::Green);
-		colors.push_back(sf::Color::White);
+		sf::RenderWindow window(sf::VideoMode(160, 144), "GPU Test");
 
 		unsigned char mushroom[] = {195, 195, 129, 189, 0, 126, 0, 126, 0, 0, 189, 189, 189, 189, 195, 195};
 		for (int i = 0; i < 16; i++)
@@ -33,22 +22,8 @@ namespace GBEmulator
 		_oam.write(2, 0);
 		_oam.write(3, 0);
 
-		auto *pixels = new sf::Uint8[8 * 8 * 4];
-
-		auto tile = getTile(0);
-		for (int i = 0; i < 64; i++) {
-			pixels[0 + i * 4] = colors[tile[i]].r;
-			pixels[1 + i * 4] = colors[tile[i]].g;
-			pixels[2 + i * 4] = colors[tile[i]].b;
-			pixels[3 + i * 4] = colors[tile[i]].a;
-		}
-
-		texture.create(8, 8);
-		texture.update(pixels);
-
-		sprite.setTexture(texture);
-		sprite.setPosition(80 * 4, 72* 4);
-		sprite.setScale(6, 6);
+		for (int i = 0; i < 1; i++)
+			_textures.push_back(getTextureFromTile(getTile(i)));
 
 		while (window.isOpen())
 		{
@@ -56,9 +31,10 @@ namespace GBEmulator
 			while (window.pollEvent(event))
 				if (event.type == sf::Event::Closed)
 					window.close();
+
 			window.clear(sf::Color::White);
 
-			window.draw(sprite);
+			window.draw(getSprite());
 
 			window.display();
 		}
@@ -70,21 +46,9 @@ namespace GBEmulator
 		for (int i = id; i < id + 16; i += 2) {
 			auto layer1 = decToBin(_vram.read(i));
 			auto layer2 = decToBin(_vram.read(i + 1));
-
 			for (int j = 0; j < 8; j++)
 				tile[j + 4 * i] = (layer1[j] * 2) + layer2[j];
 		}
-
-		int k = 1;
-		for (int i = 0; i < 64; i++) {
-			std::cout << tile[i];
-			if (k == 8) {
-				std::cout << std::endl;
-				k = 0;
-			}
-			k++;
-		}
-		std::cout << std::endl;
 		return tile;
 	}
 
@@ -97,5 +61,52 @@ namespace GBEmulator
 			nbr = nbr / 2;
 		}
 		return result;
+	}
+
+	sf::Texture GPU::getTextureFromTile(std::vector<int> tile) {
+		sf::Texture texture;
+		auto *pixels = new sf::Uint8[8 * 8 * 4];
+
+		texture.create(8, 8);
+		for (int i = 0; i < 64; i++) {
+			pixels[0 + i * 4] = this->COLORS[tile[i]].r;
+			pixels[1 + i * 4] = this->COLORS[tile[i]].g;
+			pixels[2 + i * 4] = this->COLORS[tile[i]].b;
+			pixels[3 + i * 4] = this->COLORS[tile[i]].a;
+		}
+		texture.update(pixels);
+		return texture;
+	}
+
+	sf::Sprite GPU::getSprite() {
+		sf::Sprite sprite;
+
+		float x = _oam.read(0);
+		float y = _oam.read(1);
+		int type = _oam.read(2);
+		int flag = _oam.read(3);
+
+		sprite.setTexture(_textures[type]);
+		sprite.setPosition(x, y);
+
+		return sprite;
+	}
+
+	unsigned char GPU::readVRAM(unsigned short address) {
+
+		return _vram.read(address);
+	}
+
+	void GPU::writeVRAM(unsigned short address, unsigned char value) {
+		_vram.write(address, value);
+	}
+
+	unsigned char GPU::readOAM(unsigned short address) {
+
+		return _oam.read(address);
+	}
+
+	void GPU::writeOAM(unsigned short address, unsigned char value) {
+		_oam.write(address, value);
 	}
 }
--- src/ProcessingUnits/GPU.hpp
+++ src/ProcessingUnits/GPU.hpp
@@ -8,19 +8,35 @@
 #ifndef GBEMULATOR_GPU_HPP
 #define GBEMULATOR_GPU_HPP
 
+#include <SFML/Graphics.hpp>
+#include <memory>
 #include "../Memory/Memory.hpp"
 
 namespace GBEmulator
 {
+	#define VRAM_SIZE 8 * 1024
+	#define OAM_SIZE 159
+
 	class GPU {
+		std::vector<sf::Color> COLORS = {sf::Color::Black, sf::Color::Magenta, sf::Color::Green, sf::Color::White};
+
 	private:
 		Memory _vram;
 		Memory _oam;
+		std::vector<sf::Texture> _textures;
 
 	public:
 		GPU();
+		unsigned char readVRAM(unsigned short address);
+		void writeVRAM(unsigned short address, unsigned char value);
+		unsigned char readOAM(unsigned short address);
+		void writeOAM(unsigned short address, unsigned char value);
+
+	private:
 		std::vector<int> getTile(std::size_t id);
 		std::vector<int> decToBin(int nbr);
+		sf::Texture getTextureFromTile(std::vector<int> tile);
+		sf::Sprite getSprite();
 	};
 }
 
