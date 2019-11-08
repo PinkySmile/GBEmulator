bc9dfbd1fd740ff64e95680f841b1e46d64bc3a4
agrellier <alberto.grellier@epitech.eu>
2019-11-08 17:49:43 +0100

    Add tests for CPU::write

---
+++ tests/TestCpu/Test_write.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2019
+** GBEmulator
+** File description:
+** Test_write.cpp
+*/
+
+#include <criterion/criterion.h>
+#include "../communism.hpp"
+#include "../TestComponents.hpp"
+
+Test(write, wram)
+{
+	Tests::GBTest gb;
+
+	gb.cpu.write(0xC000, 0x69);
+
+	cr_assert(gb.cpu._ram._memory[0] == 0x69);
+
+	gb.cpu.write(0xC005, 0x07);
+
+	cr_assert(gb.cpu._ram._memory[5] == 0x07);
+}
+
+Test(write, echo_ram)
+{
+	Tests::GBTest gb;
+
+	gb.cpu.write(0xC000, 0x69);
+
+	cr_assert(gb.cpu._ram._memory[0] == 0x69);
+
+	gb.cpu.write(0xC005, 0x07);
+
+	cr_assert(gb.cpu._ram._memory[5] == 0x07);
+}
