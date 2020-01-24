7a66e48389585f4dd9980fc6d51889eb4c7f24c3
Andgel <andgel.halley@epitech.eu>
2020-01-24 12:01:07 +0100

    Added doc for Cablelink interfaces

--- src/CableLink/BgbProtocolNetworkInterface.hpp
+++ src/CableLink/BgbProtocolNetworkInterface.hpp
@@ -15,19 +15,25 @@
 
 namespace GBEmulator::Network
 {
+	//! @brief L'interface CableLink utilisant le protocole de l'émulateur BGB.
 	class BGBProtocolCableInterface : public CableInterface {
 	private:
+		//! @brief Le ProtocolHandler utilisée pour communiquer via le protocole de BGB.
 		BGBHandler _handler;
 		void _sync(unsigned cycles) override;
 		void _sendByte(unsigned char byte) override;
 
 	public:
+		//! @brief Constructeur.
 		BGBProtocolCableInterface();
-		BGBProtocolCableInterface(const BGBProtocolCableInterface &) = delete;
-		~BGBProtocolCableInterface() = default;
-		BGBProtocolCableInterface &operator=(const BGBProtocolCableInterface &) = delete;
+		//! @brief Connecte à un émulateur avec une ip ou un nom de domaine et son port.
+		//! @param host Nom de domaine ou ip du serveur.
+		//! @param port Port d'écoute du serveur.
 		void connect(const std::string &host, unsigned short port);
+		//! @brief Héberge un serveur sur un port.
+		//! @param port Port d'écoute.
 		void host(unsigned short port);
+		//! @brief Se déconnecte de l'éumateur distant.
 		void disconnect();
 	};
 }
--- src/CableLink/CableInterface.hpp
+++ src/CableLink/CableInterface.hpp
@@ -10,25 +10,61 @@
 
 namespace GBEmulator::Network
 {
+	//! @brief L'interface émulant le CableLink.
+	//! Pour plus d'inforamtions: http://problemkaputt.de/pandocs.htm#serialdatatransferlinkcable
 	class CableInterface {
 	private:
+		//! @brief Syncronise l'émulateur avec la console distante.
+		//! @param cycles Le nombre de cycles CPU qui se sont écoulés.
+		//! @note Cette fonction peut bloquer si l'émulateur est en avance par rapport à la console distante.
 		virtual void _sync(unsigned cycles) = 0;
+
+		//! @brief Envoie un byte de donnée.
+		//! @param byte Le byte qui doit être envoyé.
 		virtual void _sendByte(unsigned char byte) = 0;
 
 	protected:
+		//! @brief Est-ce qu'une interruption doit être envoyé.
 		bool _needInterrupt = false;
+		//! @brief Est-ce que l'horloge utilisée est celle de la console distante.
 		bool _isExternal = false;
+		//! @brief Est-ce qu'un transfert est actif.
 		bool _isTransfering = false;
 
 	public:
+		//! @brief Le byte utilisé pour l'I/O ($FF01)
 		unsigned char byte = 0xFF;
 
+		//! @brief Change le byte de controle.
+		//! @param c_byte Le nouveau byte de controle
+		//! @details
+		//! Bit 7 - Transfert démarré (0=Pas de transfert, 1=Transfert en cours)
+		//! Bit 0 - Horloge utilisée (0=Horloge externe, 1=Horloge interne)
 		void setControlByte(unsigned char c_byte);
+
+		//! @brief Construit le byte de controle.
+		//! @return Le byte de controle.
+		//! @details
+		//! Bit 7 - Transfert démarré (0=Pas de transfert, 1=Transfert en cours)
+		//! Bit 0 - Horloge utilisée (0=Horloge externe, 1=Horloge interne)
 		unsigned char getControlByte() const;
+
+		//! @brief Est-ce que l'horloge utilisée est celle de la console distante.
+		//! @return false si l'horloge utilisée est l'horloge interne. Sinon, true.
 		bool isExternal() const;
+
+		//! @brief Est-ce qu'une interruption à été déclanchées.
 		bool triggerInterrupt();
+
+		//! @brief Est-ce que le transfert est toujours en cours.
+		//! @return true si un transfert en cours. Sinon, false.
 		bool isTransfering() const;
+
+		//! @brief Met à jour l'état de l'interface.
+		//! @param cycles Le nombre de cycles CPU écoulés.
 		void update(unsigned int cycles);
+
+		//! @brief Active le transfert.
 		void transfer();
 	};
 }
