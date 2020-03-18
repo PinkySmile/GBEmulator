86f12c9daba12f5db33a4df58f0cf518db1382e7
Gegel85 <Gegel85@laposte.net>
2020-03-18 17:52:00 +0100

    Fixed sounds

--- src/ProcessingUnits/SoundChannel/ModulableWaveChannel.cpp
+++ src/ProcessingUnits/SoundChannel/ModulableWaveChannel.cpp
@@ -22,13 +22,11 @@ namespace GBEmulator::SoundChannel
 	{
 		static std::vector<unsigned char>	raw(4096LLU);
 
-		for (int i = 0; i < 512; i += 1) {
+		for (int i = 0; i < 4096 / 2; i += 1) {
 			unsigned char wpRam = this->_wpRAM.read(i % 16);
 
-			for (int j = 0; j < 4; j++) {
-				raw[i * 8 + j] = (wpRam >> 4) / 15 * 254 - 127;
-				raw[i * 8 + 4 + j] = (wpRam & 0b00001111) / 15 * 254 - 127;
-			}
+			raw[i * 2] = (wpRam >> 4U)  << 4U | 0xFU;
+			raw[i * 2] = (wpRam & 0xFU) << 4U | 0xFU;
 		}
 		return (raw);
 	}
@@ -76,7 +74,8 @@ namespace GBEmulator::SoundChannel
 	void ModulableWaveChannel::setSoundOnOff(unsigned char value)
 	{
 		this->_soundOn = value >> 7U;
-		this->_sound.setVolume(this->_soundOn * this->_initialVolume * 100.f / 15);
+		this->_restart &= this->_soundOn;
+		this->_sound.setVolume(this->_soundOn * this->_initialVolume * 100.f / 31);
 	}
 
 	void ModulableWaveChannel::setSoundLength(unsigned char value)
@@ -88,14 +87,14 @@ namespace GBEmulator::SoundChannel
 	{
 		this->_waveOutputLevel = (value & 0b01100000U) >> 5U;
 		switch (this->_waveOutputLevel) {
-			case 0:
-				this->_initialVolume = 0;
-				break;
-			default:
-				this->_initialVolume = 15 / (1U << (this->_waveOutputLevel - 1));
-				break;
+		case 0:
+			this->_initialVolume = 0;
+			break;
+		default:
+			this->_initialVolume = 0x1FU >> (this->_waveOutputLevel - 1);
+			break;
 		}
-		this->setVolume(this->_initialVolume * 100.f / 15);
+		this->setVolume(this->_initialVolume * 100.f / 31);
 	}
 
 	void ModulableWaveChannel::write(unsigned char address, unsigned char value)
@@ -104,4 +103,21 @@ namespace GBEmulator::SoundChannel
 		this->_genWave = true;
 	}
 
+	void ModulableWaveChannel::setLowFrequency(unsigned char value)
+	{
+		this->_frequency = (this->_frequency & 0b11100000000U) | value;
+		this->_realFrequency = 65536.f / (2048 - this->_frequency);
+		this->_sound.setPitch(this->_realFrequency / 2760);
+	}
+
+	void ModulableWaveChannel::setRestartOptions(unsigned char value)
+	{
+		unsigned short val = (value & 0b00000111U) << 8U;
+
+		this->_restart = value >> 7U;
+		this->_restartType = (value & 0b01000000U) >> 6U;
+		this->_frequency = (this->_frequency & 0b00011111111U) | val;
+		this->_realFrequency = 65536.f / (2048 - this->_frequency);
+		this->_sound.setPitch(this->_realFrequency / 2760);
+	}
 }
