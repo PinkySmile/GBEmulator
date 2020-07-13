936ce70e3e581e380ecfb9b4fff74e0fa5179eba
Gegel85 <Gegel85@laposte.net>
2020-07-13 20:54:28 +0200

    Improved speed

--- src/ProcessingUnits/GPU.cpp
+++ src/ProcessingUnits/GPU.cpp
@@ -195,8 +195,15 @@ namespace GBEmulator
 
 			sprite.y = this->_oam.read(i) - 16;
 			sprite.x = this->_oam.read(i + 1) - 8;
+
+			if (sprite.x == 0 || sprite.x >= 168 || sprite.y == 0 || sprite.y >= 160)
+				continue;
+
 			sprite.texture_id = this->_oam.read(i + 2);
 			sprite.flags = this->_oam.read(i + 3);
+
+			if (this->_control & 0b00000100U)
+				sprite.texture_id &= 0xFEU;
 
 			unsigned char *tile = this->_tiles + sprite.texture_id * 64;
 
