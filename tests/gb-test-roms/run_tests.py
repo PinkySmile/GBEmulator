3213667b04271b2c78dd4416d771acd172384343
Andgel <andgel.halley@epitech.eu>
2021-06-24 04:10:06 +0900

    Modified test script to use asyncio instead of threads

--- tests/gb-test-roms/run_tests.py
+++ tests/gb-test-roms/run_tests.py
@@ -1,156 +1,125 @@
 import os
 import sys
-import socket
-import subprocess
-import select
-import threading
-import time
+from asyncio import subprocess
+from typing import Tuple, List
+import asyncio
 
-base_port = 10800
-S_IFMT = 0o170000
-S_IFDIR = 0o40000
 
-flags = ["--no-error", "--no-bootrom", "--no-display", "--no-audio", "--max-speed", "--listen"]
-results = {}
-threads = []
-total = 0
-longest = 0
-p = False
+base_port = 15800
 
-def getEmuTestResult(sock):
-	buffer = []
+async def getEmuTestResult(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> bytearray:
+	buffer = bytearray()
 	timeSpent = 0
 	timeSpent2 = 0
 	while True:
-		ready = select.select([sock.fileno()], [], [], 0.25)[0]
-		if not ready and ("Passed" in "".join(buffer) or "Failed" in "".join(buffer)):
-			return buffer
-		if not ready:
-			timeSpent2 += 0.25
-			if len([r for r in "".join(buffer).split('\n') if r]) > 1:
-				timeSpent += 0.25
-				if timeSpent > 10:
-					return buffer
-			if timeSpent2 >= 40:
-				buffer.append("\nTimed out after 10 seconds\n")
-				return buffer
-			continue
-		else:
+		try:
+			b = await asyncio.wait_for(reader.readexactly(8), 0.25)
 			timeSpent = 0
 			timeSpent2 = 0
-		d = sock.recv(8)
-		if not d:
+		except asyncio.exceptions.TimeoutError:
+			if b"Passed" in buffer or b"Failed" in buffer:
+				return buffer
+			timeSpent2 += 0.25
+			if buffer.count(10) > 2:
+				timeSpent += 0.25
+				if timeSpent > 10:
+					print("Time !", buffer.count(10))
+					return buffer
+			if timeSpent2 >= 40:
+				buffer += b"\nTimed out after 10 seconds\n"
+				return buffer
+			continue
+		if not b:
 			return buffer
-		if d[0] == 104:
-			buffer.append(chr(d[1]))
-			sock.send(bytes([105, 0, 0x80, 0, 0, 0, 0, 0]))
+		if b[0] == 104:
+			buffer.append(b[1])
+			writer.write(bytes([105, 0, 0x80, 0, 0, 0, 0, 0]))
+			await writer.drain()
 
 
-def executeTest(romPath, port):
-	times = 0
-	sock = socket.socket()
-	args = [emuPath, *flags, str(port), romPath]
-	#args = [emuPath, "-nrabml", str(port), romPath]
-	process = subprocess.Popen(args, 0, emuPath, subprocess.DEVNULL, subprocess.DEVNULL)
-	while True:
-		times += 1
-		try:
-			sock.connect(("localhost", port))
-			break
-		except ConnectionRefusedError:
-			time.sleep(0.1)
-		if times >= 30:
-			results[romPath.replace("/", "_").replace(" ", "_")] = "Cannot connect to emulator\n"
-			print("Cannot connect to emulator......")
-			return
-	sock.send(bytes([1, 1, 4, 0, 0, 0, 0, 0]))
+async def executeTest(emuPath: str, romPath: str, port: int) -> Tuple[str, str]:
+	args = ["--no-error", "--no-bootrom", "--no-display", "--no-audio", "--max-speed", "--listen", str(port), romPath]
+	#args = ["-nrabml", str(port), romPath]
+	print(f"Running {romPath} in {emuPath} on port {port}")
+	process = await subprocess.create_subprocess_exec(emuPath, *args)
 	try:
-		buffer = getEmuTestResult(sock)
-	except BrokenPipeError:
-		buffer = "BrokenPipeError\n"
-	sock.close()
+		reader, writer = await asyncio.wait_for(asyncio.open_connection(host="localhost", port=port), 3)
+	except asyncio.TimeoutError as e:
+		print("Cannot connect to emulator......", e)
+		return romPath.replace("/", "_").replace(" ", "_"), "Cannot connect to emulator\n"
+	writer.write(bytes([1, 1, 4, 0, 0, 0, 0, 0]))
+	await writer.drain()
+
+	buffer = await getEmuTestResult(reader, writer)
 	process.terminate()
-	old = buffer
-	buffer = []
+	old = buffer.decode("ASCII")
+	result = []
 	count = 0
 	while count < len(old):
 		if count + 1 < len(old) and old[count] == old[count + 1] and (old[count] == ' ' or old[count] == '\n'):
 			while count + 1 < len(old) and old[count] == old[count + 1]:
 				count += 1
-			buffer.append('\n')
+			result.append('\n')
 		else:
-			buffer.append(old[count])
+			result.append(old[count])
 		count += 1
-	results[romPath.replace("/", "_").replace(" ", "_")] = "".join(buffer)
-	global p
-	while p:
-		pass
-	p = True
-	global total
-	total += 1
-	print("\033[A" + str(total) + "/" + str(len(threads)))
-	p = False
+	print(f"Finished {romPath}")
+	return romPath.replace("/", "_").replace(" ", "_"), "".join(result)
 
 
-class TestThread(threading.Thread):
-	def __init__(self, path, port):
-		super().__init__(None, None)
-		self.path = path
-		self.port = port
-
-	def run(self):
-		executeTest(self.path, self.port)
-
-
-def executeTestSuite(directory):
-	global base_port
-	for rom in sorted(os.listdir(directory)):
-		if rom[-3:].lower() == ".gb" or rom[-4:].lower() == ".gbc":
-			port = base_port
-			global longest
+async def executeTestSuite(emuPath: str, directory: str, port: int) -> Tuple[int, List[Tuple[str, str]]]:
+	longest = 0
+	tasks = []
+	for i, rom in enumerate(sorted(os.listdir(directory))):
+		if rom.lower().endswith(".gb") or rom.lower().endswith(".gbc"):
 			longest = max(longest, len(directory + "/" + rom))
-			thr = TestThread(directory + "/" + rom, port)
-			thr.start()
-			threads.append(thr)
-			base_port += 1
+			tasks.append(executeTest(emuPath, directory + "/" + rom, port + i))
+	return longest, dict(await asyncio.gather(*tasks))
 
 
-def testEmulator(path):
-	expected = "Usage: " + path + " rom.gb [-dnrmba] [-l <port>] [-c <ip:port>] [-g auto|dmg|gbc]"
-	f = os.popen(path + " --help", "r")
-	if f.read(len(expected)) != expected:
+async def testEmulator(path: str) -> None:
+	expected = bytes("Usage: " + path + " rom.gb [-dnrmba] [-l <port>] [-c <ip:port>] [-g auto|dmg|gbc]", "ASCII")
+	process = await asyncio.create_subprocess_exec(path, "--help", stdout=subprocess.PIPE)
+	if (read := await process.stdout.read(len(expected))) != expected:
 		print(path + ": GBEmulator didn't return the proper help message")
+		print(read)
 		exit(1)
-	f.close()
+	try:
+		await asyncio.wait_for(process.wait(), 1)
+	except asyncio.TimeoutError as e:
+		print(e)
+		exit(1)
 	print(path + " seems ok")
 
 
-if len(sys.argv) != 2:
-	print("Usage:", sys.argv[0], "<GBEmulator executable>", sep=" ")
-	exit(1)
+async def main():
+	longestName = 0
 
+	if len(sys.argv) != 2:
+		print("Usage:", sys.argv[0], "<GBEmulator executable>")
+		exit(1)
 
-emuPath = sys.argv[1]
-testEmulator(emuPath)
-for elem in sorted(os.listdir()):
-	if (os.stat(elem).st_mode & S_IFMT) == S_IFDIR:
-		executeTestSuite(elem)
+	emuPath = sys.argv[1]
+	await testEmulator(emuPath)
 
-p = True
-print("0/" + str(len(threads)))
-p = False
-for thread in threads:
-	thread.join()
+	tasks = [executeTestSuite(emuPath, suite, base_port + i * 100) for i, suite in enumerate(sorted(os.listdir("."))) if os.path.isdir(suite)]
+	results = await asyncio.gather(*tasks)
 
-passed = 0
-failed = 0
-for name, result in sorted(results.items(), key=lambda a: a[0]):
-	if "Passed" in result:
-		passed += 1
-		print(("{:<" + str(longest) + "}: \033[32mPassed\033[0m").format(name))
-	else:
-		failed += 1
-		print(("{:<" + str(longest) + "}: \033[31mFailed\033[0m. Rom output:").format(name))
-		print(result)
+	result = {}
+	for length, r in results:
+		longestName = max(longestName, length)
+		result.update(r)
 
-print("Total:", passed, "\033[32mPassed\033[0m,", failed, "\033[31mFailed\033[0m")
+	passed = 0
+	failed = 0
+	for name, result in sorted(result.items(), key=lambda a: a[0]):
+		if "Passed" in result:
+			passed += 1
+			print(f"{{:<{longestName}}}: \033[32mPassed\033[0m".format(name))
+		else:
+			failed += 1
+			print(f"{{:<{longestName}}}: \033[31mFailed\033[0m. Rom output:".format(name))
+			print(result)
+	print("Total:", passed, "\033[32mPassed\033[0m,", failed, "\033[31mFailed\033[0m")
+
+asyncio.run(main())
